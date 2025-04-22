38b107d6d2c2e0461f161896219396253069080a
David Hale <dhale@caltech.edu>
2025-04-22 16:50:15 -0700

    adds more functionality --  o starts adding command processing in camera_server and  o consequentially adds functions to xxx_interface.h/.cpp

--- camerad/archon_interface.cpp
+++ camerad/archon_interface.cpp
@@ -1,5 +1,12 @@
+/**
+ * @file    archon_interface.cpp
+ * @brief   implementation of Archon Interface
+ * @author  David Hale <dhale@astro.caltech.edu>
+ *
+ */
+
 #include "archon_interface.h"
-#include <iostream>
+#include "archon_controller.h"
 
 namespace Camera {
 
@@ -11,6 +18,275 @@ namespace Camera {
     const std::string function("Camera::ArchonInterface::myfunction");
     logwrite(function, "Archon's implementation of Camera::myfunction");
   }
+  ArchonInterface::~ArchonInterface() {
+  }
+
+
+  /***** Camera::ArchonInterface::abort ***************************************/
+  /**
+   * @brief
+   * @param[in]  args
+   * @param[out] retstring
+   * @return     ERROR | NO_ERROR
+   *
+   */
+  long ArchonInterface::abort( const std::string args, std::string &retstring ) {
+    const std::string function("Camera::ArchonInterface::abort");
+    logwrite(function, "not yet implemented");
+    return ERROR;
+  }
+  /***** Camera::ArchonInterface::abort ***************************************/
+
+
+  /***** Camera::ArchonInterface::autodir *************************************/
+  /**
+   * @brief
+   * @param[in]  args
+   * @param[out] retstring
+   * @return     ERROR | NO_ERROR
+   *
+   */
+  long ArchonInterface::autodir( const std::string args, std::string &retstring ) {
+    const std::string function("Camera::ArchonInterface::autodir");
+    logwrite(function, "not yet implemented");
+    return ERROR;
+  }
+  /***** Camera::ArchonInterface::autodir *************************************/
+
+
+  /***** Camera::ArchonInterface::basename ************************************/
+  /**
+   * @brief      set or get the image basename
+   * @param[in]  args       requested base name
+   * @param[out] retstring  current base name
+   * @return     ERROR | NO_ERROR | HELP
+   *
+   */
+  long ArchonInterface::basename( const std::string args, std::string &retstring ) {
+    const std::string function("Camera::ArchonInterface::basename");
+    long error=NO_ERROR;
+
+    // Help
+    //
+    if (args=="?" || args=="help") {
+      retstring = CAMERAD_BASENAME;
+      retstring.append( " [ <name> ]\n" );
+      retstring.append( "  set or get image basename\n" );
+      return HELP;
+    }
+
+    // Base name cannot contain a "/" because that would be a subdirectory,
+    // and subdirectories are not checked here, only by imdir command.
+    //
+    if ( args.find('/') != std::string::npos ) {
+      logwrite( function, "ERROR basename cannot contain '/' character" );
+      error = ERROR;
+    }
+    // if name is supplied the set the image name
+    else if ( !args.empty() ) {
+      this->camera_info.base_name = args;
+      error = NO_ERROR;
+    }
+
+    // In any case, log and return the current value.
+    //
+    logwrite(function, "base name is "+std::string(this->camera_info.base_name));
+    retstring = this->camera_info.base_name;
+
+    return error;
+  }
+  /***** Camera::ArchonInterface::basename ************************************/
+
+
+  /***** Camera::ArchonInterface::bias ****************************************/
+  /**
+   * @brief      set a bias voltage
+   * @param[in]  args       contains <module> <chan> <voltage>
+   * @param[out] retstring  
+   * @return     ERROR | NO_ERROR | HELP
+   *
+   */
+  long ArchonInterface::bias( const std::string args, std::string &retstring ) {
+    const std::string function("Camera::ArchonInterface::bias");
+    std::stringstream message;
+    std::vector<std::string> tokens;
+    std::stringstream biasconfig;
+    int module;
+    int channel;
+    float voltage;
+    float vmin, vmax;
+    bool readonly=true;
+
+    // Help
+    //
+    if (args=="?" || args=="help") {
+      retstring = CAMERAD_BIAS;
+      retstring.append( " <module> <chan> <voltage>\n" );
+      retstring.append( "  set a bias voltage\n" );
+      return HELP;
+    }
+
+    // must have loaded firmware
+    //
+    if ( ! this->controller.is_firmwareloaded ) {
+      logwrite( function, "ERROR firmware not loaded" );
+      return ERROR;
+    }
+
+    Tokenize(args, tokens, " ");
+
+    if (tokens.size() == 2) {
+      readonly = true;
+
+    } else if (tokens.size() == 3) {
+      readonly = false;
+
+    } else {
+      message.str(""); message << "incorrect number of arguments: " << args << ": expected module channel [voltage]";
+      logwrite( function, message.str() );
+      return ERROR;
+    }
+
+    try {
+      module  = std::stoi( tokens[0] );
+      channel = std::stoi( tokens[1] );
+      if (!readonly) voltage = std::stof( tokens[2] );
+    }
+    catch (const std::exception &e) {
+      message.str(""); message << "ERROR parsing \"" << args << "\": expected <module> <channel> [ voltage ]: "
+                               << e.what();
+      logwrite( function, message.str() );
+      return ERROR;
+
+    }
+
+    // Check that the module number is valid
+    //
+    if ( (module < 0) || (module > nmods) ) {
+      message.str(""); message << "module " << module << ": outside range {0:" << nmods << "}";
+      logwrite( function, message.str() );
+      return ERROR;
+    }
+
+    // Use the module type to get LV or HV Bias
+    // and start building the bias configuration string.
+    //
+    switch ( this->controller.modtype[ module-1 ] ) {
+      case 0:
+        message.str(""); message << "module " << module << " not installed";
+        logwrite( function, message.str() );
+        return ERROR;
+        break;
+      case 3:  // LVBias
+      case 9:  // LVXBias
+        biasconfig << "MOD" << module << "/LV";
+        vmin = -14.0;
+        vmax = +14.0;
+        break;
+      case 4:  // HVBias
+      case 8:  // HVXBias
+        biasconfig << "MOD" << module << "/HV";
+        vmin =   0.0;
+        vmax = +31.0;
+        break;
+      default:
+        message.str(""); message << "module " << module << " not a bias board";
+        logwrite( function, message.str() );
+        return ERROR;
+        break;
+    }
+
+    // Check that the channel number is valid
+    // and add it to the bias configuration string.
+    //
+    if ( (channel < 1) || (channel > 30) ) {
+      message.str(""); message << "bias channel " << module << ": outside range {1:30}";
+      logwrite( function, message.str() );
+      return ERROR;
+    }
+    if ( (channel > 0) && (channel < 25) ) {
+      biasconfig << "LC_V" << channel;
+    }
+    if ( (channel > 24) && (channel < 31) ) {
+      channel -= 24;
+      biasconfig << "HC_V" << channel;
+    }
+
+    if ( (voltage < vmin) || (voltage > vmax) ) {
+      message.str(""); message << "bias voltage " << voltage << ": outside range {" << vmin << ":" << vmax << "}";
+      logwrite( function, message.str() );
+      return ERROR;
+    }
+
+    // Locate this line in the configuration so that it can be written to the Archon
+    //
+    std::string key   = biasconfig.str();
+    std::string value = std::to_string(voltage);
+    bool changed      = false;
+    long error;
+
+    // If no voltage suppled (readonly) then just read the configuration and exit
+    //
+    if (readonly) {
+      message.str("");
+      error = this->get_configmap_value(key, voltage);
+      if (error != NO_ERROR) {
+        message << "reading bias " << key;
+
+      } else {
+        retstring = std::to_string(voltage);
+        message << "read bias " << key << "=" << voltage;
+      }
+      logwrite( function, message.str() );
+      return error;
+    }
+
+    // Write the config line to update the bias voltage
+    //
+    error = this->controller.write_config_key(key.c_str(), value.c_str(), changed);
+
+    // Now send the APPLYMODx command
+    //
+    std::stringstream applystr;
+    applystr << "APPLYMOD"
+             << std::setfill('0')
+             << std::setw(2)
+             << std::hex
+             << (module-1);
+
+    if (error == NO_ERROR) error = this->send_cmd(applystr.str());
+
+    if (error != NO_ERROR) {
+      message << "writing bias configuration: " << key << "=" << value;
+
+    } else if (!changed) {
+      message << "bias configuration: " << key << "=" << value <<" unchanged";
+
+    } else {
+      message << "updated bias configuration: " << key << "=" << value;
+    }
+
+    logwrite(function, message.str());
+
+    return error;
+  }
+  /***** Camera::ArchonInterface::bias ****************************************/
+
+
+  /***** Camera::ArchonInterface::bin *****************************************/
+  /**
+   * @brief
+   * @param[in]  args
+   * @param[out] retstring
+   * @return     ERROR | NO_ERROR
+   *
+   */
+  long ArchonInterface::bin( const std::string args, std::string &retstring ) {
+    const std::string function("Camera::ArchonInterface::bin");
+    logwrite(function, "not yet implemented");
+    return ERROR;
+  }
+  /***** Camera::ArchonInterface::bin *****************************************/
 
   long Camera::ArchonInterface::connect_controller( const std::string args, std::string &retstring ) {
     const std::string function("Camera::ArchonInterface::connect_controller");
--- camerad/archon_interface.h
+++ camerad/archon_interface.h
@@ -10,15 +10,24 @@
 #include "camera_interface.h"          //!< defines Camera::Interface base class
 
 namespace Camera {
+  class Controller;
+
   class ArchonInterface : public Interface {
+    friend Controller;
+
     public:
       ArchonInterface();
       ~ArchonInterface() override;
 
-      // these virtual functions are inherited by the Camera::Interface class
-      // and implemented in archon_interface.cpp
+      // These are virtual functions inherited by the Camera::Interface base class
+      // and have their own controller-specific implementations which are
+      // implemented in archon_interface.cpp.
       //
-      void myfunction() override;
+      long abort( const std::string args, std::string &retstring ) override;
+      long autodir( const std::string args, std::string &retstring ) override;
+      long basename( const std::string args, std::string &retstring ) override;
+      long bias( const std::string args, std::string &retstring ) override;
+      long bin( const std::string args, std::string &retstring ) override;
       long connect_controller( const std::string args, std::string &retstring ) override;
       long disconnect_controller( const std::string args, std::string &retstring ) override;
       long exptime( const std::string args, std::string &retstring ) override;
