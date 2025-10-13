6e947e68153de2e0a515f8b06f52a5f94d3bdf28
David Hale <dhale@caltech.edu>
2025-10-13 16:58:22 -0700

    Creates a Controller base class and ArchonController derived class, and moves controller-related functions to that new class. Load ACF functions are working.

--- camerad/camera_information.h
+++ camerad/camera_information.h
@@ -6,7 +6,8 @@ namespace Camera {
   class Information {
     friend class CameraInterface;
     public:
-      Common::FitsKeys userkeys;
+      Common::FitsKeys userkeys;            //!< FITS keys for command line use
+      Common::FitsKeys systemkeys;          //!< FITS keys for system use
 
       std::string base_name;                //!< base image name
 //    std::map<int, std::string> firmware;  //!< firmware file for given controller @TODO support multiple controllers
